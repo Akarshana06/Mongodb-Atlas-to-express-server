9f3168220884554e2297ae8e45a92268a633ed25
Akarshana06 <akarshana027@gmail.com>
2025-02-08 10:45:40 +0530

    the port runs

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const { resolve } = require('path');
-
+const connectDatabase = require('./express')
+require('dotenv').config();
 const app = express();
-const port = 3010;
+const port = process.env.PORT;
 
 app.use(express.static('static'));
 
 app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
 });
+connectDatabase();
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
